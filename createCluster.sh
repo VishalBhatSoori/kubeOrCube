4fef5e4ee4698cafccaca891a6e892108666bafc
Vishal Bhat Soori <vishalbhatsoori@gmail.com>
2025-07-14 06:11:21 +0000

    corrected the config.yml file that creates the cluster

--- createCluster.sh
+++ createCluster.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo"Stopping nginx running on port 80"
+echo "Stopping nginx running on port 80"
 systemctl stop nginx
 
-echo"Deleting the master and worker nodes running before shutting down"
-kind delete cluster kind-my-cluster
+echo "Deleting the master and worker nodes running before shutting down"
+kind delete cluster --name=my-cluster
 
-echo"Creating the kind cluster named my-cluster with config.yml configurations"
+echo "Creating the kind cluster named my-cluster with config.yml configurations"
 kind create cluster --name=my-cluster --config=config.yml
 
